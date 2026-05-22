8aaaeb14738e53dc3ff55b709bf00befdf1e262d
Breno <usuario@breno.local>
2026-05-21 21:27:16 -0300

    base de testes

--- Tests/test_endpoint.py
+++ Tests/test_endpoint.py
@@ -1,8 +1,61 @@
 import pytest
 from fastapi.testclient import TestClient
-from main import app
+from app.main import app
 
 client = TestClient(app)
 
-def test_endpointe_pokeapi():
-    pass
+@pytest.fixture(scope="module")
+def charmander():
+    """
+    Fixture para criar um pokemon charmander (usando só de modelo para lembrar o que deve ser retornado pela pokeapi.co)
+    """
+    return {
+        "name": "charmander",
+        "id": 4,
+        "height": 0.6,
+        "weight": 8.5,
+        "types": ["fire"],
+        "level": 5,
+        "sprites": {
+            "front_default": "",
+            "back_default": "",
+        }
+    }
+
+def test_endpoint_all_pokemons():
+    """
+    endpoint /pokemons deve retornar um json de pokemons da pokeapi.co
+    """
+    response = client.get("/pokemons")
+
+    assert response.json() == {
+        "data": "",
+        "pagination": {
+            "total": 0,
+            "limite": "",
+            "offset": "",
+            "next": "",
+            "previous": None
+        }
+    }
+
+    assert response.status_code == 200
+
+def test_endpoint_pokemon_por_id():
+    """
+    endpoint /pokemons/{id} deve retornar um json de pokemons da pokeapi.co
+    """
+    response = client.get("/pokemons/1")
+
+    assert response.json() == {
+        "data": "",
+        "pagination": {
+            "total": 0,
+            "limite": "",
+            "offset": "",
+            "next": "",
+            "previous": None
+        }
+    }
+
+    assert response.status_code == 200
--- app/main.py
+++ app/main.py
@@ -2,5 +2,7 @@ from fastapi import FastAPI
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from typing import BaseModel
+import pydantic
+import requests
 
 app = FastAPI()
