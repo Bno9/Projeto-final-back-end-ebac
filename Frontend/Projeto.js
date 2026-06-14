d9a5da17bc172112a9c2a31e9649e6b271ec9c30
Bno9 <breno_live2005@hotmail.com>
2026-06-14 19:12:40 -0300

    melhoria na interface

--- Frontend/Projeto.js
+++ Frontend/Projeto.js
@@ -1,32 +1,37 @@
-const botao1 = document.getElementById("btn1");
-const botao2 = document.getElementById("btn2");
-const titulo = document.getElementById("titulo");
-const id_pokeapi = document.getElementById("id_poke")
+const botao_all_pokeapi = document.getElementById("btn1");
+const botao_pokeapi_id = document.getElementById("btn2");
+const infopokeapi = document.getElementById("infopokeapi");
+const id_pokeapi = document.getElementById("id_poke");
+const front = document.getElementById("front_img_pokemon");
+const back = document.getElementById("back_img_pokemon");
 
-
-botao2.addEventListener("click", () => {
+botao_pokeapi_id.addEventListener("click", () => {
     const id = Number(id_pokeapi.value);
 
     if(id <= 0){
         return;
         }
 
-    fetch(`http://0.0.0.0:8000/pokeapi/${id}`)  
+    fetch(`http://0.0.0.0:8000/pokeapi/${id}`)  //atualmente busco no local, mas preciso mudar pra buscar no render
         .then(response => response.json())
         .then(data => {
-            titulo.textContent = JSON.stringify(data, null, 2);
-            document.getElementById("front_img_pokemon").src = data.sprites.front_default;
-            document.getElementById("back_img_pokemon").src = data.sprites.back_default;
-
+            infopokeapi.textContent = JSON.stringify(data, null, 2);
+            infopokeapi.style.border = "1px solid black";
+            front.src = data.sprites.front_default;
+            back.src = data.sprites.back_default;
+            front.style.display = "block";
+            back.style.display = "block";
         });
 });
 
-botao1.addEventListener("click", () => {
+botao_all_pokeapi.addEventListener("click", () => {
     fetch(`http://0.0.0.0:8000/pokeapi/all`)
         .then(response => response.json())
         .then(data => {
-            titulo.textContent = JSON.stringify(data, null, 2);
-            document.getElementById("front_img_pokemon").src = null;
-            document.getElementById("back_img_pokemon").src = null;
+            infopokeapi.textContent = JSON.stringify(data, null, 2);
+            infopokeapi.style.border = "1px solid black";
+            front.style.display = "none";
+            back.style.display = "none";
+
         });
 });
